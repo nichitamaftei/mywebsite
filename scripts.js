c9d20a8a48d44445e035ce9be407cec97ab26a1d
Nichita Maftei <nichitamaftei04@gmail.com>
2025-09-18 19:21:20 +0100

    Added a new Certifications section
    Added my AWS CCP & Docker certificates within the new section

--- scripts.js
+++ scripts.js
@@ -72,5 +72,6 @@ document.addEventListener('DOMContentLoaded', function() {
     applyScrollEffect('.project-item');          
     applyScrollEffect('.report-item');           
     applyScrollEffect('.contact-info-container');
+     applyScrollEffect('.trophy-card');
 
 });
